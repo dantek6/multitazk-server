545f6aac6b7f8aafe629d329cb06b594743d745f
dantek6 <daniel.fernandez@alumnos.upla.cl>
2023-03-30 17:17:45 -0300

    Eliminación de Try cath, cambio de api key para equipo Linux, cambio de PORT

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,11 @@ const app = express();
 
 env.config();
 
-app.use(cors());
 app.use(bodyParser.json());
+app.use(cors());
 
 const config = new Configuration({
-    organization: "org-iZTG3fUXueYaxrLsiOaBuhJf",
+    // organization: "org-iZTG3fUXueYaxrLsiOaBuhJf",
     apiKey: process.env.OPENAI_API_KEY
 })
 
@@ -22,25 +22,21 @@ const openai = new OpenAIApi(config);
 //     res.send("Hello WorldAAH");
 // });
 
-app.post('/', async (req, res) => {
+app.post('/chat', async (req, res) => {
     const {prompt} = req.body
 
-    try {
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `${prompt}`,
-            max_tokens: 512,
-            temperature: 0
-        })
-        res.send(response.data.choices[0].text)
-        // res.json({message: response.data.choices[0].text})
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `${prompt}`,
+        max_tokens: 512,
+        temperature: 0
+    })
+    res.send(response.data.choices[0].text)
+    // res.json({message: response.data.choices[0].text})
 
-    } catch (error) {
-        console.log(error)
-        res.send(error).status(400)
-    }
+
 });
 
-const PORT = 8020;
+const PORT = 8080;
 
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
