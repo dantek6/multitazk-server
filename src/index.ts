f6166e84903dbe90106cf9258dc9e724eda77417
dantek <daniel.fernandez@alumnos.upla.cl>
2023-05-06 14:13:18 -0400

    Función para ejecutar Querys

--- src/index.ts
+++ src/index.ts
@@ -42,52 +42,57 @@ app.get("/users", (req, res) => {
   res.send("Lista de usuarios:");
 });
 
-const PORT = process.env.PORT;
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-//Rellenar datos del modelo:
-const taskWrite = new TaskWriteModel({
-  // id: "1",
-  title: "Tarea 1",
-  description: "Descripción de la tarea 1",
-  date: new Date(),
-  timeMin: 10,
-  groupId: "1",
-  lengthMin: 10,
-  points: 10,
-  createdAt: new Date(),
-  updatedAt: new Date(),
-});
-
-const user = new UserModel({
-  id: "1",
-  name: "Usuario 1",
-  email: "jondoe@gmail.com",
-  password: "123456",
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  groups: [
-    {
-      groupId: "1",
-      isAdmin: true,
-      level: 1,
-      experience: 0,
-    },
-    {
-      groupId: "2",
-      isAdmin: false,
-      level: 1,
-      experience: 0,
-    },
-  ],
-  taskId: ["1", "2", "3", "4", 5],
-});
-
-// console.log(taskWrite);
-console.log(user);
-
 //Capturar error de Base de Datos si hay
 run().catch((err: Error) => {
   console.error("An error occurred:", err);
 });
+
+const PORT = process.env.PORT;
+
+//Rellenar datos del modelo:
+async function executeQueries() {
+  const taskWrite = new TaskWriteModel({
+    // id: "1",
+    title: "Tarea 1",
+    description: "Descripción de la tarea 1",
+    date: new Date(),
+    timeMin: 10,
+    groupId: "1",
+    lengthMin: 10,
+    points: 10,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+
+  const user = new UserModel({
+    id: "1",
+    name: "Usuario 1",
+    email: "jondoe@gmail.com",
+    password: "123456",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    groups: [
+      {
+        groupId: "1",
+        isAdmin: true,
+        level: 1,
+        experience: 0,
+      },
+      {
+        groupId: "2",
+        isAdmin: false,
+        level: 1,
+        experience: 0,
+      },
+    ],
+    taskId: ["1", "2", "3", "4", "5"],
+  });
+  // await taskWrite.save();
+  //await user.save();
+  // console.log(taskWrite);
+  console.log(user);
+}
+
+executeQueries();
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
