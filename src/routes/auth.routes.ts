186dbccdfaea2aef3b1e036205b9c02163d2bfa2
dantek <daniel.fernandez@alumnos.upla.cl>
2023-06-28 15:08:43 -0400

    Pequeña modificación en el archivo de rutas, verifyToken como método GET

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -10,7 +10,7 @@ router.post('/register', validateSchema(registerSchema), register);
 router.post('/login', validateSchema(loginSchema), login);
 router.post('/logout', logout);
 
-router.post('/verifyToken', verifyToken);
+router.get('/verifyToken', verifyToken);
 
 router.get('/profile', authRequired, profile);
 
