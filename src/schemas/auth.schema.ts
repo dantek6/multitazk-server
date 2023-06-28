2999be23dbe0361740a890f5c8ff03bb64799e49
dantek <daniel.fernandez@alumnos.upla.cl>
2023-06-28 03:03:34 -0400

    Esquema para complementar middleware

---
+++ src/schemas/auth.schema.ts
@@ -0,0 +1,41 @@
+import { z } from 'zod';
+
+export const registerSchema = z.object({
+    username: z.string({
+            required_error: "El nombre de usuario es requerido"
+        }),
+    email: z
+        .string({
+            required_error: "El correo electrónico es requerido"
+        })
+        .email({
+            message: "El correo electrónico no es válido"
+        }),
+    password: z
+        .string({
+            required_error: "La contraseña es requerida"
+        })
+        .min(6, {
+            message: "La contraseña debe tener al menos 6 caracteres"
+        }),
+});
+
+export const loginSchema = z.object({
+    // username: z.string({
+    //         required_error: "El nombre de usuario es requerido"
+    //     }),
+    email: z
+        .string({
+            required_error: "El correo electrónico es requerido"
+        })
+        .email({
+            message: "El correo electrónico no es válido"
+        }),
+    password: z
+        .string({
+            required_error: "La contraseña es requerida"
+        })
+        .min(6, {
+            message: "La contraseña debe tener al menos 6 caracteres"
+        }),
+});
